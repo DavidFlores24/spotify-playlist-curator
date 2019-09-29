1d76c74668b14c80d57dc6d98025eb5dedc7be7d
David Flores <david.fgardens@gmail.com>
2019-09-29 21:30:42 +0100

    chore: enabled css modules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-
-  );
+  return <h1></h1>;
 }
 
 export default App;
