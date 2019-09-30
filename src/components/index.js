681ca205f24c7ac2d645c24295f6affce4859b65
David Flores <david.fgardens@gmail.com>
2019-09-30 17:26:48 +0100

    feat: export new components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
 import { PlaylistItem } from "./playlistItem";
+import { trackItem } from "./trackItem";
+import { playlist } from "./playlist";
 
 export { PlaylistItem };
+export { trackItem as TrackItem };
+export { playlist as Playlist };
