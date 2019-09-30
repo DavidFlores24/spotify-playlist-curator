617bfee0dc1feee7e9478129c9feb190bd90348a
David Flores <david.fgardens@gmail.com>
2019-09-30 13:34:47 +0100

    feat: display playlistItems on CreateaPlaylist page

--- src/containers/CreatePlaylist/index.js
+++ src/containers/CreatePlaylist/index.js
@@ -1,40 +1,60 @@
 import React, { Component } from "react";
 
 import { getFromSpotify, getCookie } from "../../utils";
+import { PlaylistItem } from "../../components";
+
+import styles from "./CreatePlaylist.css";
 
 export class CreatePlaylist extends Component {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    this.state = {
-      playlists: []
-    };
+		this.state = {
+			playlists: [],
+			selectedPlaylists: []
+		};
 
-    this.getPlaylists().then(playlists =>
-      this.setState({ playlists: playlists })
-    );
-  }
+		this.getPlaylists().then(playlists =>
+			this.setState({ playlists: playlists })
+		);
+	}
 
-  async getPlaylists() {
-    const token = getCookie("token");
-    const header = {
-      Authorization: `Bearer ${token}`
-    };
-    return getFromSpotify("me/playlists", header).then(res => {
-      return res.items;
-    });
-  }
+	async getPlaylists() {
+		const token = getCookie("token");
+		const header = {
+			Authorization: `Bearer ${token}`
+		};
+		return getFromSpotify("me/playlists", header).then(res => {
+			return res.items;
+		});
+	}
 
-  render() {
-    const playlists = this.state.playlists.map((playlist, index) => {
-      return <li key={index}>{playlist.name}</li>;
-    });
+	onToggle = playlistKey => {
+		const playlistToAdd = this.state.playlists[playlistKey];
 
-    return (
-      <>
-        <h1>Got the Playlists!</h1>
-        <ul>{playlists}</ul>
-      </>
-    );
-  }
+		console.log(playlistToAdd);
+
+		const toggledPlaylists = this.state.selectedPlaylists;
+		toggledPlaylists.push(playlistToAdd);
+
+		this.setState({ selectedPlaylists: toggledPlaylists });
+	};
+
+	render() {
+		this.playlistItems = this.state.playlists.map((playlist, index) => (
+			<PlaylistItem
+				name={playlist.name}
+				key={index}
+				onToggle={() => this.onToggle(index)}
+			></PlaylistItem>
+		));
+
+		return (
+			<>
+				<h1>Got the Playlists!</h1>
+				<div className={styles.playlists}>{this.playlistItems}</div>
+				<div className={styles.button}>Create new Playlist</div>
+			</>
+		);
+	}
 }
