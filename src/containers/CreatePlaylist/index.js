4ec456c586e49f05619c642ca0d7834804673601
David Flores <david.fgardens@gmail.com>
2019-09-30 22:03:22 +0100

    fix: update createPlaylist function

--- src/containers/CreatePlaylist/index.js
+++ src/containers/CreatePlaylist/index.js
@@ -7,153 +7,119 @@ import styles from "./CreatePlaylist.css";
 
 const token = getCookie("token");
 const header = {
-	Authorization: `Bearer ${token}`
+  Authorization: `Bearer ${token}`
 };
 
 export class CreatePlaylist extends Component {
-	constructor() {
-		super();
+  constructor() {
+    super();
 
-		this.state = {
-			playlists: [],
-			selectedPlaylists: [],
-			candidateTracks: [],
-			newPlaylist: {
-				duration: 600000,
-				numberOfTracks: 0,
-				name: "",
-				tracks: []
-			}
-		};
+    this.state = {
+      playlists: [],
+      selectedPlaylists: [],
+      newPlaylist: {
+        duration: 600000,
+        numberOfTracks: 0,
+        name: "",
+        tracks: []
+      }
+    };
 
-		this.getPlaylists().then(playlists =>
-			this.setState({ playlists: playlists })
-		);
-	}
+    this.getPlaylists().then(playlists =>
+      this.setState({ playlists: playlists })
+    );
+  }
 
-	async getPlaylists() {
-		return getFromSpotify("me/playlists", header).then(res => res.items);
-	}
+  async getPlaylists() {
+    return getFromSpotify("me/playlists", header).then(res => res.items);
+  }
 
-	onToggle = playlistKey => {
-		const playlistToAdd = this.state.playlists[playlistKey];
+  onToggle = playlistKey => {
+    const playlistToAdd = this.state.playlists[playlistKey];
 
-		const toggledPlaylists = this.state.selectedPlaylists;
-		toggledPlaylists.push(playlistToAdd);
+    const toggledPlaylists = this.state.selectedPlaylists;
+    toggledPlaylists.push(playlistToAdd);
 
-		this.setState({ selectedPlaylists: toggledPlaylists });
-	};
+    this.setState({ selectedPlaylists: toggledPlaylists });
+  };
 
-	generatePlaylist = () => {
-		const promises = [];
-		this.state.selectedPlaylists.map(playlist =>
-			promises.push(this.getPlaylistTracks(playlist))
-		);
+  generatePlaylist = () => {
+    const promises = [];
+    this.state.selectedPlaylists.map(playlist =>
+      promises.push(this.getPlaylistTracks(playlist))
+    );
 
-		this.setCandidateTracks(promises);
-		// this.selectTracks();
-	};
+    this.setCandidateTracks(promises);
+  };
 
-	async getPlaylistTracks(playlist) {
-		const { id } = playlist;
+  async getPlaylistTracks(playlist) {
+    const { id } = playlist;
 
-		return getFromSpotify(`playlists/${id}/tracks`, header).then(res => {
-			return { playlist, tracks: res.items };
-		});
-	}
+    return getFromSpotify(`playlists/${id}/tracks`, header).then(res => {
+      return { playlist, tracks: res.items.map(item => item.track) };
+    });
+  }
 
-	setCandidateTracks = playlistPromises => {
-		Promise.all(playlistPromises).then(res => {
-			const overshoot = 30000;
-			let coveredTime = 0;
-			const playlistTracks = [];
+  setCandidateTracks = playlistPromises => {
+    Promise.all(playlistPromises).then(res => {
+      const overshoot = 30000;
+      let coveredTime = 0;
+      const playlistTracks = [];
 
-			console.log(res);
-			while (this.state.newPlaylist.duration - overshoot > coveredTime) {
-				res.forEach(playlist => {
-					const { tracks } = playlist;
+      while (this.state.newPlaylist.duration - overshoot > coveredTime) {
+        res.forEach(playlist => {
+          let { tracks } = playlist;
 
-					tracks.sort((a, b) => b.popularity - a.popularity);
+          tracks = tracks.filter(track => track !== null);
+          tracks.sort((a, b) => b.popularity - a.popularity);
 
-					// need to use a for loop to be able to break
-					for (let i = 0; i < tracks.length; i++) {
-						const track = tracks[i];
-						const { duration_ms } = track;
-						if (
-							!playlistTracks.includes(track) &&
-							coveredTime + duration_ms <=
-								this.state.newPlaylist.duration + overshoot
-						) {
-							playlistTracks.push(track);
-							coveredTime += duration_ms;
-							break;
-						}
-					}
-				});
-			}
+          // need to use a for loop to be able to break
+          for (let i = 0; i < tracks.length; i++) {
+            const track = tracks[i];
+            const { duration_ms } = track;
+            if (
+              !playlistTracks.includes(track) &&
+              coveredTime + duration_ms <=
+                this.state.newPlaylist.duration + overshoot
+            ) {
+              playlistTracks.push(track);
+              coveredTime += duration_ms;
+              break;
+            }
+          }
+        });
+      }
 
-			const newPlaylist = this.state.newPlaylist;
-			newPlaylist.numberOfTracks = playlistTracks.length;
-			newPlaylist.tracks = playlistTracks;
+      const newPlaylist = this.state.newPlaylist;
+      newPlaylist.numberOfTracks = playlistTracks.length;
+      newPlaylist.tracks = playlistTracks;
 
-			this.setState({ newPlaylist: newPlaylist });
-		});
-	};
+      this.setState({ newPlaylist: newPlaylist });
+    });
+  };
 
-	selectTracks = () => {
-		const overshoot = 30000;
-		let coveredTime = 0;
-		const playlistTracks = [];
+  addPlaylistToSpotify = () => {};
 
-		this.state.candidateTracks.forEach(track => {
-			const { duration_ms } = track;
+  render() {
+    this.playlistItems = this.state.playlists.map((playlist, index) => (
+      <PlaylistItem
+        name={playlist.name}
+        key={index}
+        onToggle={() => this.onToggle(index)}
+      ></PlaylistItem>
+    ));
 
-			if (
-				!playlistTracks.includes(track) &&
-				coveredTime + duration_ms <= this.state.newPlaylist.duration + overshoot
-			) {
-				playlistTracks.push(track);
-				coveredTime += duration_ms;
-			}
-		});
+    const { name, tracks } = this.state.newPlaylist;
 
-		console.log(playlistTracks);
-
-		const newPlaylist = this.state.newPlaylist;
-		newPlaylist.numberOfTracks = playlistTracks.length;
-		newPlaylist.tracks = playlistTracks;
-
-		console.log(newPlaylist);
-
-		this.setState({ newPlaylist: newPlaylist });
-	};
-
-	addPlaylistToSpotify = () => {};
-
-	render() {
-		this.playlistItems = this.state.playlists.map((playlist, index) => (
-			<PlaylistItem
-				name={playlist.name}
-				key={index}
-				onToggle={() => this.onToggle(index)}
-			></PlaylistItem>
-		));
-
-		const { name, tracks } = this.state.newPlaylist;
-
-		return (
-			<>
-				<h1>Got the Playlists!</h1>
-				<div className={styles.playlists}>{this.playlistItems}</div>
-				<div className={styles.button} onClick={this.generatePlaylist}>
-					Create new Playlist
-				</div>
-				{this.state.newPlaylist.tracks !== [] ? (
-					<Playlist name={name} tracks={tracks} />
-				) : (
-					""
-				)}
-			</>
-		);
-	}
+    return (
+      <>
+        <h1>Got the Playlists!</h1>
+        <div className={styles.playlists}>{this.playlistItems}</div>
+        <div className={styles.button} onClick={this.generatePlaylist}>
+          Create new Playlist
+        </div>
+        <Playlist name={name} tracks={tracks} />
+      </>
+    );
+  }
 }
