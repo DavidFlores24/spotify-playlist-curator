076ee066bfd29927d5dc5fbbe875ff6c44823de7
David Flores <david.fgardens@gmail.com>
2019-10-08 09:30:47 +0100

    style: use html default checkbox for playlistItems

--- src/components/playlistItem/index.js
+++ src/components/playlistItem/index.js
@@ -6,10 +6,7 @@ export const PlaylistItem = props => {
 	return (
 		<div className={classes.item}>
 			<div className={classes.label}>{props.name}</div>
-			<label className={classes.switch}>
-				<input type="checkbox" onClick={props.onToggle} />
-				<span className={classes.round}></span>
-			</label>
+			<input type='checkbox' onClick={props.onToggle} className={classes.switch}></input>
 		</div>
 	);
 };
