fd1bf120918be1b7a8312835a47c07c89ea8c5b0
David Flores <david.fgardens@gmail.com>
2019-10-08 09:27:55 +0100

    fix: Redirect to login upon opening app

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import { Home } from "./containers/Home";
 import { Login } from "./containers/Login";
 import { CreatePlaylist } from "./containers/CreatePlaylist";
 
+import styles from "./App.css";
+
 function App() {
   return (
-    <div className="App">
+    <div className={styles.app}>
       <BrowserRouter>
         <Switch>
-          <Route path="/login" component={Login} />
           <Route path="/home" component={Home} />
           <Route path="/create-playlist" component={CreatePlaylist} />
+          <Route path='/' component={Login} />
         </Switch>
       </BrowserRouter>
     </div>
