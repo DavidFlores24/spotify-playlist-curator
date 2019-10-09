ac23b46adb29f16b9310ec2ebf782dd85005d58b
David Flores <david.fgardens@gmail.com>
2019-10-09 18:37:05 +0100

    refactor(CreatePlaylist): Use new playlistGenerationUtils on CreatePlaylist page

--- src/containers/CreatePlaylist/index.js
+++ src/containers/CreatePlaylist/index.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 
-import { getFromSpotify, getCookie, postToSpotify } from "../../utils";
+import { getCookie } from "../../utils";
+import {
+	getPlaylistsFromSpotify as getPlaylists,
+	postToSpotify
+} from "../../utils/spotifyUtils";
+import { generatePlaylist } from "../../utils/playlistGenerationUtils";
 import { Button, Header, PlaylistItem, Playlist } from "../../components";
 
 import styles from "./CreatePlaylist.css";
@@ -26,15 +31,7 @@ export class CreatePlaylist extends Component {
 			}
 		};
 
-		this.getPlaylists().then(playlists =>
-			this.setState({ playlists: playlists })
-		);
-	}
-
-	async getPlaylists() {
-		return getFromSpotify("me/playlists", header).then(res => {
-			return res ? res.items : [];
-		});
+		getPlaylists().then(playlists => this.setState({ playlists: playlists }));
 	}
 
 	onToggle = playlistKey => {
@@ -44,68 +41,6 @@ export class CreatePlaylist extends Component {
 		toggledPlaylists.push(playlistToAdd);
 
 		this.setState({ selectedPlaylists: toggledPlaylists });
-	};
-
-	generatePlaylist = () => {
-		if (this.state.selectedPlaylists === []) {
-			return;
-		}
-
-		const promises = [];
-		this.state.selectedPlaylists.map(playlist =>
-			promises.push(this.getPlaylistTracks(playlist))
-		);
-
-		this.setCandidateTracks(promises);
-	};
-
-	async getPlaylistTracks(playlist) {
-		const { id } = playlist;
-
-		return getFromSpotify(`playlists/${id}/tracks`, header).then(res => {
-			return res ? { playlist, tracks: res.items.map(item => item.track) } : {};
-		});
-	}
-
-	setCandidateTracks = playlistPromises => {
-		Promise.all(playlistPromises).then(res => {
-			const overshoot = 30000;
-			let coveredTime = 0;
-			const playlistTracks = [];
-
-			while (this.state.newPlaylist.duration - overshoot > coveredTime) {
-				res.forEach(playlist => {
-					let { tracks } = playlist;
-
-					tracks = tracks.filter(track => track !== null);
-					tracks.sort((a, b) => b.popularity - a.popularity);
-
-					// need to use a for loop to be able to break
-					for (let i = 0; i < tracks.length; i++) {
-						const track = tracks[i];
-						const { duration_ms } = track;
-						if (
-							!playlistTracks.includes(track) &&
-							coveredTime + duration_ms <=
-								this.state.newPlaylist.duration + overshoot
-						) {
-							playlistTracks.push(track);
-							coveredTime += duration_ms;
-							break;
-						}
-					}
-				});
-			}
-
-			const newPlaylist = this.state.newPlaylist;
-			newPlaylist.numberOfTracks = playlistTracks.length;
-			newPlaylist.tracks = playlistTracks;
-
-			this.setState({
-				newPlaylist: newPlaylist,
-				showNewPlaylist: true
-			});
-		});
 	};
 
 	addPlaylistToSpotify = () => {
@@ -128,10 +63,6 @@ export class CreatePlaylist extends Component {
 			});
 		});
 	};
-
-	async getUser() {
-		return getFromSpotify("me", header);
-	}
 
 	onBlur = e => {
 		const playlistName = e.target.value;
@@ -172,6 +103,24 @@ export class CreatePlaylist extends Component {
 		});
 
 		document.getElementById("durationSpan").innerText = value;
+	};
+
+	createPlaylist = () => {
+		generatePlaylist(
+			this.state.selectedPlaylists,
+			this.state.newPlaylist.duration
+		).then(res => {
+			const { newPlaylist } = this.state;
+			const { tracks, numberOfTracks } = res;
+
+			newPlaylist.tracks = tracks;
+			newPlaylist.numberOfTracks = numberOfTracks;
+
+			this.setState({
+				newPlaylist: newPlaylist,
+				showNewPlaylist: true
+			});
+		});
 	};
 
 	render() {
@@ -216,10 +165,7 @@ export class CreatePlaylist extends Component {
 						<div className={styles.column}>{this.three}</div>
 					</div>
 					<div className={styles.button}>
-						<Button
-							onClick={this.generatePlaylist}
-							label="Create new Playlist"
-						/>
+						<Button onClick={this.createPlaylist} label="Create new Playlist" />
 					</div>
 				</div>
 
