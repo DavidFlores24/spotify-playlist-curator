6183d586280da72494e751208ef27e30a4f0aecc
David Flores <david.fgardens@gmail.com>
2019-10-09 18:57:59 +0100

    refactor(playlistGenerationUtils): add addPlaylistToSpotify function

--- src/utils/playlistGenerationUtils/index.js
+++ src/utils/playlistGenerationUtils/index.js
@@ -1,7 +1,9 @@
+import { addPlaylistToSpotify } from "./addPlaylistToSpotify";
 import { generatePlaylist } from "./generatePlaylist";
 import { getCandidateTracks } from "./getCandidateTracks";
 import { getPlaylistTracks } from "./getPlaylistTracks";
 
+export { addPlaylistToSpotify };
 export { generatePlaylist };
 export { getCandidateTracks };
 export { getPlaylistTracks };
