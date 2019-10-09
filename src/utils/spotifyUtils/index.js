835f720e0d0156f6e4621fff36e8bc191cee1f93
David Flores <david.fgardens@gmail.com>
2019-10-09 18:35:31 +0100

    refactor: export spotifyUtils

---
+++ src/utils/spotifyUtils/index.js
@@ -0,0 +1,9 @@
+import { getFromSpotify } from "./getFromSpotify";
+import { getPlaylistsFromSpotify } from "./getPlaylistsFromSpotify";
+import { getUser } from "./getUser";
+import { postToSpotify } from "./postToSpotify";
+
+export { getFromSpotify };
+export { getPlaylistsFromSpotify };
+export { getUser };
+export { postToSpotify };
