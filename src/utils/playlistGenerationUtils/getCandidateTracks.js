0efcd58d9827e86208165ab548d42759ddf3419e
David Flores <david.fgardens@gmail.com>
2019-10-09 18:34:19 +0100

    refactor: create getCandidateTracks function

---
+++ src/utils/playlistGenerationUtils/getCandidateTracks.js
@@ -0,0 +1,50 @@
+/*
+    Resolve the tracks promises, sort them and return the most popular ones from each Playlist
+
+    @params:
+    playlistPromises: array of promises which resolution contains an object of { playlist, tracks }
+    newPlaylistDuration: time in ms which the new playlist should last
+
+    returns:
+    array with sorted tracks
+*/
+
+export async function getCandidateTracks(
+	playlistPromises,
+	newPlaylistDuration
+) {
+	let playlistTracks = [];
+
+	playlistTracks = await Promise.all(playlistPromises).then(res => {
+		const overshoot = 30000;
+		let coveredTime = 0;
+		const playlistTracks = [];
+
+		while (newPlaylistDuration - overshoot > coveredTime) {
+			res.forEach(playlist => {
+				let { tracks } = playlist;
+
+				tracks = tracks.filter(track => track !== null);
+				tracks.sort((a, b) => b.popularity - a.popularity);
+
+				// need to use a for loop to be able to break
+				for (let i = 0; i < tracks.length; i++) {
+					const track = tracks[i];
+					const { duration_ms } = track;
+					if (
+						!playlistTracks.includes(track) &&
+						coveredTime + duration_ms <= newPlaylistDuration + overshoot
+					) {
+						playlistTracks.push(track);
+						coveredTime += duration_ms;
+						break;
+					}
+				}
+			});
+		}
+
+		return playlistTracks;
+	});
+
+	return playlistTracks;
+}
