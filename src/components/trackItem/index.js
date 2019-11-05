da8b11fcb2c17cda10575fcc4b15f810b530cdd9
David Flores <david.fgardens@gmail.com>
2019-11-05 21:54:19 +0000

    feat(track-item): pass onSwitch function

--- src/components/trackItem/index.js
+++ src/components/trackItem/index.js
@@ -5,10 +5,22 @@ import { ReplacementsModal } from "../index";
 import classes from "./trackItem.css";
 
 export const trackItem = props => {
+	const { track } = props;
 	return (
 		<div className={classes.item}>
-			{props.name}
-			<ReplacementsModal replacementTracks={props.replacementTracks} />
+			<div className={classes.track}>
+				<div className={classes.trackName}>{track.track.name}</div>
+				{/*TODO: add switch component
+				onClick = open Modal*/}
+				<div className={classes.replace} onClick={() => {}}>
+					Switch this Track
+				</div>
+			</div>
+			<ReplacementsModal
+				originalTrack={track}
+				replacementTracks={props.replacementTracks}
+				onSwitch={props.onSwitch}
+			/>
 		</div>
 	);
 };
