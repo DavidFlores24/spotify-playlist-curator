db76de6248930f46d41da01684b84381098d75e4
David Flores <david.fgardens@gmail.com>
2019-11-05 18:40:48 +0000

    feat(trackItem): pass replacementTracks to replacementsModal

--- src/components/trackItem/index.js
+++ src/components/trackItem/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 
+import { ReplacementsModal } from "../index";
+
 import classes from "./trackItem.css";
 
 export const trackItem = props => {
-	return <div className={classes.item}>{props.name}</div>;
+	return (
+		<div className={classes.item}>
+			{props.name}
+			<ReplacementsModal replacementTracks={props.replacementTracks} />
+		</div>
+	);
 };
