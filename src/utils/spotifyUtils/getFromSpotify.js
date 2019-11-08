907d206b7e9b0aeb0b5dc95533b4e9fe434c4221
David Flores <david.fgardens@gmail.com>
2019-11-08 17:03:37 +0000

    refactor(spotify-utils): add error handling to getFromSpotify

--- src/utils/spotifyUtils/getFromSpotify.js
+++ src/utils/spotifyUtils/getFromSpotify.js
@@ -4,16 +4,22 @@ import { stringify } from "qs";
 const baseUri = "https://api.spotify.com/v1";
 
 export async function getFromSpotify(endpoint, header = {}, queryParams = {}) {
-  const uri = `${baseUri}/${endpoint}${stringify(queryParams, {
-    addQueryPrefix: true
-  })}`;
+	const uri = `${baseUri}/${endpoint}${stringify(queryParams, {
+		addQueryPrefix: true
+	})}`;
 
-  const response = await fetch(uri, {
-    method: "GET",
-    headers: header
-  });
+	const response = await fetch(uri, {
+		method: "GET",
+		headers: header
+	});
 
-  if (response.status === 200) {
-    return response.json();
-  }
+	try {
+		if (response.status === 200) {
+			return response.json();
+		} else {
+			throw new Error("Service not currently available");
+		}
+	} catch (error) {
+		throw new Error(error);
+	}
 }
