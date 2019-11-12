9f52a65fe64743f790e2900949954fb9014d6d5a
David Flores <david.fgardens@gmail.com>
2019-11-12 21:49:39 +0000

    fix(error-boundary): use error message as label

--- src/hoc/ErrorBoundary/index.js
+++ src/hoc/ErrorBoundary/index.js
@@ -4,41 +4,44 @@ import { ErrorOverlay } from "../../components/index";
 import styles from "./ErrorBoundary.css";
 
 export class ErrorBoundary extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			hasError: false,
-			error: null,
-			errorInfo: ""
-		};
-	}
+    this.state = {
+      hasError: false,
+      error: null,
+      errorInfo: ""
+    };
+  }
 
-	componentDidCatch(error, errorInfo) {
-		this.setState({
-			hasError: true,
-			error: error,
-			errorInfo: errorInfo
-		});
+  componentDidCatch(error, errorInfo) {
+    this.setState({
+      hasError: true,
+      error: error,
+      errorInfo: errorInfo
+    });
 
-		// TODO: log the error somewhere in the db
-	}
+    // TODO: log the error somewhere in the db
+  }
 
-	dismiss() {
-		this.setState({
-			hasError: false,
-			error: null,
-			errorInfo: ""
-		});
-	}
+  dismiss() {
+    this.setState({
+      hasError: false,
+      error: null,
+      errorInfo: ""
+    });
+  }
 
-	render() {
-		if (this.state.hasError) {
-			return (
-				<ErrorOverlay message={this.state.errorInfo} dismiss={this.dismiss} />
-			);
-		} else {
-			return this.props.children;
-		}
-	}
+  render() {
+    if (this.state.hasError) {
+      return (
+        <ErrorOverlay
+          message={this.state.error.message}
+          dismiss={this.dismiss}
+        />
+      );
+    } else {
+      return this.props.children;
+    }
+  }
 }
