2e8a44920b806021cd7448a92e50b38302c923a5
David Flores <david.fgardens@gmail.com>
2019-11-12 21:48:59 +0000

    refactor(playlist): change playlist to be a component

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,11 @@
 import { PlaylistItem } from "./playlistItem";
 import { trackItem } from "./trackItem";
-import { playlist } from "./playlist";
 import { button } from "./button";
 import { header } from "./header";
 
 export { PlaylistItem };
 export { trackItem as TrackItem };
-export { playlist as Playlist };
+export { Playlist } from "./playlist";
 export { button as Button };
 export { header as Header };
 export { replacementsModal as ReplacementsModal } from "./replacementsModal";
--- src/components/playlist/index.js
+++ src/components/playlist/index.js
@@ -1,49 +1,170 @@
-import React from "react";
+import React, { Component } from "react";
 
 import { getCookie } from "../../utils";
-import { TrackItem, Header, Button } from "../index";
+import { getTrackFromSpotify as getTrack } from "../../utils/spotifyUtils";
+import { addPlaylistToSpotify as addPlaylist } from "../../utils/playlistGenerationUtils";
+
+import { TrackItem, Header, Button, Overlay } from "../index";
 
 import styles from "./playlist.css";
 
-export const playlist = props => {
-	const tracks = props.tracks.map((track, index) => {
-		const playlistCookie = getCookie(`playlist_${track.playlistId}`);
-		const replacementTracks =
-			playlistCookie === "" ? [] : JSON.parse(playlistCookie);
+export class Playlist extends Component {
+  constructor(props) {
+    super(props);
 
-		return (
-			<TrackItem
-				track={track}
-				key={index}
-				index={index}
-				replacementTracks={replacementTracks}
-				onSwitch={props.onSwitchTrack}
-				showRecommendations={() => props.showRecommendations(index)}
-				shouldShowRecommendations={props.switchingTrackIndex === index}
-			/>
-		);
-	});
+    this.state = {
+      tracks: [],
+      name: "",
 
-	let playlistClasses = [styles.playlist];
-	if (props.show) {
-		playlistClasses.push([styles.show]);
-	}
+      show: false,
+      showOverlay: false,
+      isSwitching: false,
 
-	return (
-		<div className={playlistClasses.join(" ")}>
-			<div className={styles.header}>
-				<Header label="Your New Playlist" />
-			</div>
-			<input
-				type="text"
-				className={styles.name}
-				placeholder="Playlist name"
-				onBlur={props.onBlur}
-			/>
-			<div className={styles.tracks}>{tracks}</div>
-			<div className={styles.button}>
-				<Button label="Add Playlist to Spotify" onClick={props.onClick} />
-			</div>
-		</div>
-	);
-};
+      hasError: false,
+      error: null
+    };
+  }
+
+  switchTrack = (track, trackIndex, newTrackId, playlistId) => {
+    const { track: originalTrack } = track;
+
+    getTrack(newTrackId).then(res => {
+      const newTrack = res;
+      const tracks = [...this.state.tracks];
+
+      tracks[trackIndex] = { playlistId, track: newTrack };
+
+      this.makeOriginalTrackReplacement(originalTrack, newTrackId, playlistId);
+      this.setState({
+        tracks: tracks,
+        isSwitching: false,
+        switchingTrackIndex: null
+      });
+    });
+  };
+
+  makeOriginalTrackReplacement = (track, newTrackId, playlistId) => {
+    let replacementTracks = JSON.parse(getCookie(`playlist_${playlistId}`));
+    replacementTracks = replacementTracks.filter(({ id }) => id !== newTrackId);
+
+    const { name, artists, id } = track;
+    replacementTracks.push({
+      name,
+      artists,
+      id
+    });
+
+    document.cookie = `playlist_${playlistId}=${JSON.stringify(
+      replacementTracks
+    )}`;
+  };
+
+  showRecommendations = trackIndex => {
+    this.setState({
+      isSwitching: true,
+      switchingTrackIndex: trackIndex
+    });
+  };
+
+  createTrackItems = tracks => {
+    return tracks.map((track, index) => {
+      const playlistCookie = getCookie(`playlist_${track.playlistId}`);
+      const replacementTracks =
+        playlistCookie === "" ? [] : JSON.parse(playlistCookie);
+
+      return (
+        <TrackItem
+          key={index}
+          track={track}
+          index={index}
+          replacementTracks={replacementTracks}
+          onSwitch={this.switchTrack}
+          showRecommendations={this.showRecommendations}
+          shouldShowRecommendations={this.state.switchingTrackIndex === index}
+        />
+      );
+    });
+  };
+
+  addPlaylistToSpotify = () => {
+    try {
+      addPlaylist({ name: this.state.name, tracks: this.state.tracks });
+    } catch (error) {
+      this.setState({
+        hasError: true,
+        error: error
+      });
+    }
+
+    this.setState({ showOverlay: true });
+    setTimeout(() => this.setState({ showOverlay: false }), 5000);
+  };
+
+  setPlaylistName = e => {
+    const name = e.target.value;
+    this.setState({ name: name });
+  };
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (
+      nextProps.tracks !== prevState.tracks ||
+      nextProps.show !== prevState.show
+    ) {
+      return { tracks: nextProps.tracks, show: nextProps.show };
+    } else {
+      return null;
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    const { show, tracks } = prevProps;
+    console.log(show, tracks);
+
+    if (this.state.show !== show || this.state.tracks !== tracks) {
+      this.setState({
+        tracks: tracks,
+        show: show
+      });
+    }
+  }
+
+  render() {
+    if (this.state.hasError) {
+      throw new Error(this.state.error);
+    }
+
+    const trackItems = this.createTrackItems(this.state.tracks);
+
+    let playlistClasses = [styles.playlist];
+    if (this.state.show) {
+      playlistClasses.push(styles.show);
+    }
+
+    return (
+      <>
+        <Overlay
+          message="Your new Playlist is now on your Spotify Library! Go check it out."
+          show={this.state.showOverlay}
+        />
+        <div className={playlistClasses.join(" ")}>
+          <div className={styles.header}>
+            <Header label="Your New Playlist" />
+          </div>
+          <input
+            type="text"
+            className={styles.name}
+            placeholder="Playlist name"
+            onBlur={this.setPlaylistName}
+          />
+          <div className={styles.tracks}>{trackItems}</div>
+          <div className={styles.button}>
+            <Button
+              label="Add Playlist to Spotify"
+              onClick={this.addPlaylistToSpotify}
+            />
+          </div>
+        </div>
+      </>
+    );
+  }
+}
