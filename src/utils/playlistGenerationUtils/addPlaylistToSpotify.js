fca31064c50addf304293987b8e58a05de07ad42
David Flores <david.fgardens@gmail.com>
2019-11-15 16:03:55 +0000

    refactor(add-playlist-to-spotify): fix indentation

--- src/utils/playlistGenerationUtils/addPlaylistToSpotify.js
+++ src/utils/playlistGenerationUtils/addPlaylistToSpotify.js
@@ -3,37 +3,37 @@ import { getCookie } from "../getCookie";
 
 const token = getCookie("token");
 const header = {
-	Authorization: `Bearer ${token}`
+  Authorization: `Bearer ${token}`
 };
 
 export const addPlaylistToSpotify = playlistToAdd => {
-	validate(playlistToAdd);
+  validate(playlistToAdd);
 
-	getSpotifyUser().then(user => {
-		const { id } = user;
-		const { name, tracks } = playlistToAdd;
+  getSpotifyUser().then(user => {
+    const { id } = user;
+    const { name, tracks } = playlistToAdd;
 
-		const playlist = {
-			collaborative: false,
-			name,
-			public: true
-		};
+    const playlist = {
+      collaborative: false,
+      name,
+      public: true
+    };
 
-		postToSpotify(`users/${id}/playlists`, header, {}, playlist).then(res => {
-			const { id } = res;
-			const uris = tracks.map(track => track.track.uri);
+    postToSpotify(`users/${id}/playlists`, header, {}, playlist).then(res => {
+      const { id } = res;
+      const uris = tracks.map(track => track.track.uri);
 
-			postToSpotify(`playlists/${id}/tracks`, header, {}, { uris });
-		});
-	});
+      postToSpotify(`playlists/${id}/tracks`, header, {}, { uris });
+    });
+  });
 };
 
 const validate = ({ name, tracks }) => {
-	if (!name || name === "") {
-		throw new Error("Please give your Playlist a name.");
-	}
+  if (!name || name === "") {
+    throw new Error("Please give your Playlist a name.");
+  }
 
-	if (!tracks || tracks.length === 0) {
-		throw new Error("No tracks are being added to your Playlist.");
-	}
+  if (!tracks || tracks.length === 0) {
+    throw new Error("No tracks are being added to your Playlist.");
+  }
 };
