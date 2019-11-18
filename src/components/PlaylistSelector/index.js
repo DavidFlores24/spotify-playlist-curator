44fa2be30d0c9e9db06225bf26047702bf1e8de7
David Flores <david.fgardens@gmail.com>
2019-11-18 11:36:56 +0000

    refactor(playlist-selector): create playlist selector component

--- src/components/PlaylistSelector/index.js
+++ src/components/PlaylistSelector/index.js
@@ -1,7 +1,46 @@
-import React, { Component } from "./node_modules/react";
+import React from "react";
 
-export class PlaylistSelector extends Component {
-  constructor(props) {
-    super(props);
+import { PlaylistItem } from "../index";
+
+import styles from "./PlaylistSelector.css";
+
+const sortPlaylists = playlistItems => {
+  const one = [];
+  const two = [];
+  const three = [];
+
+  for (let i = 0; i < playlistItems.length; i++) {
+    if (i % 3 === 0) {
+      three.push(playlistItems[i]);
+    } else if (i % 2 === 0) {
+      two.push(playlistItems[i]);
+    } else {
+      one.push(playlistItems[i]);
+    }
   }
-}
+
+  return { one, two, three };
+};
+
+export const PlaylistSelector = props => {
+  const { playlists, onToggle } = props;
+  const playlistItems = playlists.map((playlist, index) => (
+    <PlaylistItem
+      name={playlist.name}
+      key={index}
+      onToggle={() => onToggle(index)}
+    />
+  ));
+
+  const { one, two, three } = sortPlaylists(playlistItems);
+
+  return (
+    <div className={styles.selector}>
+      <div className={styles.playlists}>
+        <div className={styles.column}>{one}</div>
+        <div className={styles.column}>{two}</div>
+        <div className={styles.column}>{three}</div>
+      </div>
+    </div>
+  );
+};
