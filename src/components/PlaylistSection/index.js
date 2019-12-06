e71df23a0ee1e944f651b455a913ec4c5e537658
David Flores <david.fgardens@gmail.com>
2019-12-06 16:25:11 +0000

    fix(playlist-section): use slider component

--- src/components/PlaylistSection/index.js
+++ src/components/PlaylistSection/index.js
@@ -1,19 +1,21 @@
 import React from "react";
 
-import { PlaylistParameterSelector } from "../index";
+import { PlaylistParameterSelector, Slider } from "../index";
 
 import styles from "./PlaylistSection.css";
 
 export const PlaylistSection = props => {
   return (
     <div className={styles.section}>
-      <input
-        className={styles.slider}
-        type="range"
-        min="3"
-        max="60"
-        onInput={e => props.onDurationChange(props.index, e.target.value)}
-      />
+      <div className={styles.label}>Section Duration</div>
+      <div className={styles.durationSlider}>
+        <Slider 
+          min='3' 
+          max='60' 
+          onInput={e => props.onDurationChange(props.index, e.target.value)}
+          id='duration'>
+        </Slider>
+      </div>
       <div className={styles.parameters}>
         <PlaylistParameterSelector
           sectionIndex={props.index}
